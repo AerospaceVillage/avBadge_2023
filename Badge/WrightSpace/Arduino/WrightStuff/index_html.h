36b6e9f8790c7f2b45a2fd75c46e9213f0a74794
Dan Allen <danallen@Dans-MacBook-Pro.local>
2023-04-29 17:31:33 -0400

    refactor and PoC for touch interactions.

--- Badge/WrightSpace/Arduino/WrightStuff/index_html.h
+++ Badge/WrightSpace/Arduino/WrightStuff/index_html.h
@@ -2,12 +2,15 @@
 const char *index_html_code =  "<html>\
 	<head>\
 		<meta name='viewport' content='width=device-width, initial-scale=1'>\
-<!--2FudCB0byBrbm93IG1vcmUgYWJvdXQgTk9UQU1TIGFuZCB0aGUgd2Vic2l0ZSBvdXRhZ2UgdGhhdCBtYWRlIHRoZSBuZXdzIHJlY2VudGx5PwoKTGVhcm4gbW9yZSBhYm91dCB0aGUgb3V0YWdlIGhlcmU6Cmh0dHBzOi8vd3d3LmZhYS5nb3YvbmV3c3Jvb20vZmFhLW5vdGFtLXN0YXRlbWVudAoKVGhlIE5PVEFNUyB3ZWJzaXRlIGl0c2VsZiBpcyBoZXJlOgpodHRwczovL3d3dy5ub3RhbXMuZmFhLmdvdi9kaW5zUXVlcnlXZWIv-->\
- 	<title>The Wright Stuff</title>\
+ 	  <title>The Wright Stuff</title>\
+    <style>\
+      .button { background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }\
+    </style>\
 	</head>\
 	<body bgcolor='white'>\
 		<h1>Aerospace Village</h1>\
     <ul><li>Build</li><li>Inspire</li><li>Promote</li></ul>\
-		<p>LED is %s</p>\
+		<p>Color Index: %i</p>\
+    <p>Change the satelletile <a href=\"/C\" class=\"button\">COLOR</a></p><br>\
 	</body>\
 </html>";
